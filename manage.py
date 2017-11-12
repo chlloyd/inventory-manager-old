e67862bf3e39fe9d745faaacf13524cc7ee56b76
Alexander Lloyd <a.lloyd.2@bham.ac.uk>
2017-11-12 14:15:06 +0000

    Added flask_migrate

--- manage.py
+++ manage.py
@@ -1,9 +1,22 @@
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
-from invmanager import create_app
+from invmanager import create_app, db
 
 app = create_app()
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(
+        app=app,
+        db=db
+    )
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
 
 if __name__ == '__main__':
     manager.run()
