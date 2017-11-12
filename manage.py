7bc2cd48d2fe861e51293eeb4566f575df05c5ee
Alexander Lloyd <a.lloyd.2@bham.ac.uk>
2017-11-12 14:03:14 +0000

    Added flask_manager

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
+from flask_script import Manager, Shell
+
 from invmanager import create_app
 
 app = create_app()
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
