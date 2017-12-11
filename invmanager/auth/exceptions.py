ef1f348dc6f3a952743976bd09cca09fa90d6836
Alexander Lloyd <alex.lloyd_1@hotmail.com>
2017-12-11 18:00:28 +0000

    Added create user schema

---
+++ invmanager/auth/exceptions.py
@@ -0,0 +1,5 @@
+
+
+class AuthorisationError(Exception):
+    """Exception thrown when a username/password combination is not correct.
+    """
--- invmanager/auth/schema.py
+++ invmanager/auth/schema.py
@@ -1,16 +1,49 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from .models import User
+from .exceptions import AuthorisationError
+from .models import User, db
+
+
+def create_user(name, email, password):
+    u = User()
+    u.name = name
+    u.email = email
+    u.password = password
+
+    db.session.add(u)
+    db.session.commit()
+
+    return u
+
+
+def get_user(info):
+    token = info.context.session.get('token')
+
+    if not token:
+        return
+
+    id = 1  # TODO JWT STUFF
+    user = User.query.get(id)
+    return user
 
 
 class UserType(SQLAlchemyObjectType):
     class Meta:
         model = User
+        exclude_fields = ('password_hash',)
 
 
 class AuthQuery(graphene.ObjectType):
+    me = graphene.Field(UserType)
+
     users = graphene.List(UserType)
+
+    def resolve_me(self, info):
+        user = get_user(info)
+
+        if user is None:
+            raise AuthorisationError("Incorrect username/password combination")
 
     def resolve_users(self, info):
         return User.query.all()
@@ -23,8 +56,21 @@ class Login(graphene.Mutation):
         email = graphene.String()
         password = graphene.String()
 
-    def mutate(self, info, username, password):
-        pass
+    def mutate(self, info, email, password):
+        print(info)
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, name, email, password):
+        # TODO: Check current user has admin rights
+        user = create_user(name, email, password)
+        return CreateUser(user)
 
 
 class AuthMutation(object):
